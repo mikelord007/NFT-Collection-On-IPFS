56eb08ad810502c1d1c1a91e95780c6bf12b3789
lordmike007 <manujasan23@gmail.com>
2022-12-20 05:32:52 +0530

    image not rendering bug fixed

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -178,7 +178,7 @@ export default function Home() {
           {renderButton()}
         </div>
         <div>
-          <img className={styles.image} src="./LW3punks/1.png" />
+          <img className={styles.image} src="/LW3punks/1.png" />
         </div>
       </div>
 
